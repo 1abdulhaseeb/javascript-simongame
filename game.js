770571daff48eed2e1438a1295a5911d31b760ed
Abdul Haseeb <36897158+1abdulhaseeb@users.noreply.github.com>
2021-08-30 02:20:55 +0500

    Add files via upload
    Initial Commit

---
+++ game.js
@@ -0,0 +1,106 @@
+
+var gamePattern = [];
+var userClickedPattern = [];
+var buttonColors = ["blue", "green", "yellow", "red"];
+var level;
+var count = 0;
+
+$(".start-button").click(function(){
+  level = 0;
+  $(".main-text").text("Level " + level);
+  $(".start-button").hide();
+  $(".side-text").hide();
+  computer();
+});
+
+
+$(".button").click(function(){
+  var userColor = this.id;
+  userClickedPattern.push(userColor);
+  count++;
+  var address = "sounds/" + userColor + ".mp3";
+  playSound(address);
+  animatePress(userColor);
+  if(count > level){
+    count =0;
+    checkAnswer(userClickedPattern.length - 1);
+  }
+
+});
+
+
+function checkAnswer(currentLevel){
+  if(checkEqual(currentLevel)){
+    setTimeout(function(){
+    level++;
+    $(".main-text").text("Level " + level);
+    userClickedPattern = [];
+    computer();
+  }, 1000)
+
+  }
+  else
+  {
+    playSound("sounds/wrong.mp3");
+    $("body").addClass("game-over");
+    setTimeout(function(){
+    $("body").removeClass("game-over");
+    }, 200);
+
+    $(".main-text").text("Game Over! at level " + level);
+    $(".start-button").text("Restart Game");
+    $(".start-button").show();
+    $(".start-button").click(function(){
+      startOver();
+    });
+  }
+}
+function computer(){
+  var randomColor = buttonColors[sequence()];
+  gamePattern.push(randomColor);
+  animatePress(randomColor);
+  var colorAudio = "sounds/" + randomColor + ".mp3";
+  playSound(colorAudio);
+}
+
+function sequence(){
+  var randomNumber = Math.floor(Math.random() * 4);
+  return randomNumber;
+}
+
+function animatePress(currentColor){
+  $("." + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("." + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+function playSound(address){
+  var audio = new Audio(address);
+  audio.play();
+}
+
+function checkEqual(currentLevel){
+  var flag = true;
+  for(var i=0; i<gamePattern.length; i++){
+    if(gamePattern[i]!= userClickedPattern[i])
+    {
+      flag = false;
+    }
+  }
+  if(flag == true){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+function startOver(){
+  level = 0;
+  gamePattern = [];
+  count = 0;
+  $(".main-text").text("Simon Game");
+  $(".side-text").show();
+  $(".start-button").text("Start Game");
+}
